1a0eb6a83cd86635a9d9aae39b71209ddfa51dcd
Ashis Kumar <ashishsahoo389@gmail.com>
2024-09-03 16:26:27 +0530

    Theme has been setup.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,11 @@ import type { AppProps } from "next/app";
 import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import theme from "@/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <DndProvider backend={HTML5Backend}>
         <Component {...pageProps} />
       </DndProvider>
